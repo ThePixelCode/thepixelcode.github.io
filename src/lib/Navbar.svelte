e552b69993dc70ae9832e00d630891d29c0d84a7
misterbug5 <72231627+misterbug5@users.noreply.github.com>
2023-03-22 15:39:23 -0500

    Added navbar

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import "./Navbar.css"
+    let data = [
+    ["/", "Home"],
+    ["/projects", "Projects"]
+]
+</script>
+<nav class="m-0 p-4 top-0 inset-x-0 w-full bg-gray-300 text-black dark:bg-black dark:text-white">
+	<ul class="flex flex-row">
+		{#each data as [url, title]}
+			<li class="inline-block mx-2 max-w-fit"><a href={url} class="link cursor-pointer">{title}</a></li>
+		{/each}
+	</ul>
+</nav>
