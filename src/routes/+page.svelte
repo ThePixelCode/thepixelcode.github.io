7469ef60aa10d946bded92d95ae73ffda469274e
misterbug5 <72231627+misterbug5@users.noreply.github.com>
2023-03-22 18:16:40 -0500

    Main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<script>
+<script lang="ts">
+	import type { PageData } from "./$types";
+
+
+    export let data: PageData
 </script>
+<div class="w-full h-half-screen text-center text-black">
+    <h1 class="pt-40 text-3xl">Hi 👋, I'm misterbug</h1>
+    <p>I'm a programmer and this are my projects:</p>
+</div>
+<div class="grid grid-cols-4 gap-4 m-4">
+    {#each data.cards as card}
+        <a href="{card.link}"><div class="bg-neutral-300 p-2">
+            <div>{card.title}</div>
+            <div>{card.desc}</div>
+            <div>{card.Count}</div>
+        </div></a>
+    {/each}
+</div>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,39 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
+import type { PageLoad } from "./$types"
+
+class card {
+    title: string
+    desc: string
+    link: string
+    private count: number
+
+    constructor(title: string, desc: string, link: string, count: number){
+        this.title = title
+        this.desc = desc
+        this.link = link
+        if(count < 0) throw new Error("Count cannot be negative");
+        this.count = count
+    }
+
+    
+    public get Count() : string {
+        if(this.count == 0) return "Comming Soon..."
+        return this.count == 1? "There is 1 project" : `There are ${this.count} projects`
+    }
+    
+}
+
 export const prerender = true;
+export const ssr = false
+export const load = (()=>{
+    return {
+        cards: [
+            new card("Javascript", "javascript desc", "/projects/javascript", 0),
+            new card("Python", "python desc", "/projects/python", 2),
+            new card("Java", "java desc", "/projects/java", 1),
+            new card("Rust", "rust desc", "/projects/rust", 0),
+            new card("Docker", "docker desc", "/projects/docker", 0)
+        ]
+    }
+}) satisfies PageLoad
