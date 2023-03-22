0055dcd8c57278b4edcb16ec236feb7c3fa05b09
misterbug5 <72231627+misterbug5@users.noreply.github.com>
2023-03-22 15:38:53 -0500

    Moved to SvelteKit

---
+++ src/routes/placeholder.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+
+describe("placeholder test", ()=>{
+    it("placeholder test", ()=>{
+        expect(1).toBe(1)
+    })
+})
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+//const dev = process.argv.includes("dev")
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +10,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: "docs",
+			fallback: "404.html"
+		}),
+		paths: {
+			base: ''
+		}
 	}
 };
 
