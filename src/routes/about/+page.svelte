f8e22ff382cdd7349ec59afa90dbea21d36d1f9c
misterbug5 <72231627+misterbug5@users.noreply.github.com>
2023-04-05 21:58:49 -0500

    Added About and Contact pages (Placeholders)

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<div class="text-center text-black my-4">
+    <h1 class="text-3xl">About this page</h1>
+    <p class="my-4">This page is written using <a href="https://kit.svelte.dev/" class="underline">SvelteKit</a>.</p>
+    <p class="my-4">You can find the source code <a href="https://github.com/misterbug5/misterbug5.github.io" class="underline">here</a>.</p>
+</div>
