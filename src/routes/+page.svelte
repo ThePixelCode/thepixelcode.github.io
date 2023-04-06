9701891bffe101d53835e2623d282622e3054a61
misterbug5 <72231627+misterbug5@users.noreply.github.com>
2023-04-06 15:11:32 -0500

    Fixed username change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
     export let data: PageData
 </script>
 <div class="w-full h-half-screen text-center text-black pt-[20vh]">
-    <h1 class="text-3xl">Hi 👋, I'm misterbug</h1>
+    <h1 class="text-3xl">Hi 👋, I'm ThePixelCode</h1>
     <p>I'm a programmer and this are my projects:</p>
 </div>
 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 m-4">
