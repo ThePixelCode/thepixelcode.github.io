598669dcad65f1713660df36f3afa054f8f493aa
misterbug5 <72231627+misterbug5@users.noreply.github.com>
2023-04-05 21:09:43 -0500

    Fixed overflow problems

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,11 @@
 
     export let data: PageData
 </script>
-<div class="w-full h-half-screen text-center text-black">
-    <h1 class="pt-40 text-3xl">Hi 👋, I'm misterbug</h1>
+<div class="w-full h-half-screen text-center text-black pt-[20vh]">
+    <h1 class="text-3xl">Hi 👋, I'm misterbug</h1>
     <p>I'm a programmer and this are my projects:</p>
 </div>
-<div class="grid grid-cols-4 gap-4 m-4">
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 m-4">
     {#each data.cards as card}
         <a href="{card.link}"><div class="bg-neutral-300 p-2">
             <div>{card.title}</div>
